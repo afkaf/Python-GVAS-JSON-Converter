aff72eff0296de5cb34a19bbb5ad157642a5959e
afkaf <62219143+afkaf@users.noreply.github.com>
2023-08-23 01:08:57 +0300

    Removed test code from JsonToSav.py

--- SavConverter/JsonToSav.py
+++ SavConverter/JsonToSav.py
@@ -8,10 +8,6 @@ def json_to_sav(json_string):
     for raw_property in properties:
         property_instance = assign_prototype(raw_property)
         bytestring = property_instance.to_bytes()
-        test = bytestring == test_file[last:len(bytestring)+last]
-        last += len(bytestring)
-        if not test:
-            quit()
         output.extend(bytestring)
 
     return bytes(output)
