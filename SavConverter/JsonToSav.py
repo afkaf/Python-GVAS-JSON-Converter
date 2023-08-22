abcfcc6f7e4f92d0e2160738ec7e43d72d4ac0b3
afkaf <62219143+afkaf@users.noreply.github.com>
2023-08-23 00:14:15 +0300

    Implemented SavWriter.py and JsonToSav.py
    Added code and logic to convert .json back to .sav and updated the library structure.

---
+++ SavConverter/JsonToSav.py
@@ -0,0 +1,20 @@
+from .SavProperties import *
+import json
+
+with open('ExampleSavFiles/SaveSlot.sav', 'rb') as f:
+    test_file = f.read()
+
+def json_to_sav(json_string):
+    properties = json.loads(json_string)
+    output = bytearray()
+    last = 0
+    for raw_property in properties:
+        property_instance = assign_prototype(raw_property)
+        bytestring = property_instance.to_bytes()
+        test = bytestring == test_file[last:len(bytestring)+last]
+        last += len(bytestring)
+        if not test:
+            quit()
+        output.extend(bytestring)
+
+    return bytes(output)
